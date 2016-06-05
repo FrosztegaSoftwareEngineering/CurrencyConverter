a8616996fb2165459a18476047eb279c905a3b83
Richard Frosztega <richard.frosztega@gmail.com>
2016-06-05 10:10:08 +0100

    Format converted currency for display to user in their console

--- CurrencyConverter/src/main/java/com/fse/currencyconverter/App.java
+++ CurrencyConverter/src/main/java/com/fse/currencyconverter/App.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import com.fse.currencyconverter.model.ConvertedCurrency;
+
 public class App {
 
 	public static void main(String[] args) throws IOException {
@@ -12,7 +14,12 @@ public class App {
 		final String toCode = readLine("A target ISO 4217 currency code: ");
 
 		final CurrencyConverter currencyConverter = buildApplication();
-		System.out.println(currencyConverter.convert(amount, fromCode, toCode));
+		final ConvertedCurrency convertedCurrency = currencyConverter.convert(amount, fromCode, toCode);
+		System.out.println(
+				String.format("%s %s, %s",
+						convertedCurrency.amount(),
+						convertedCurrency.currencyName(),
+						convertedCurrency.countryName()));
 	}
 
 	private static String readLine(String message) throws IOException {
