6c1c56b4e59f67c3f6acc69214495c43e30d5d5b
Richard Frosztega <richard.frosztega@gmail.com>
2016-06-05 09:30:04 +0100

    Read input from StdIn and display conversion

--- CurrencyConverter/src/main/java/com/fse/currencyconverter/App.java
+++ CurrencyConverter/src/main/java/com/fse/currencyconverter/App.java
@@ -1,8 +1,26 @@
 package com.fse.currencyconverter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
 
-	public static void main(String[] args) {
-		System.out.println("Hello CurrencyConverter App");
+	public static void main(String[] args) throws IOException {
+		final String amount = readLine("A source currency amount in the format x.yy: ");
+		final String fromCode = readLine("A source ISO 4217 currency code: ");
+		final String toCode = readLine("A target ISO 4217 currency code: ");
+
+		final CurrencyConverter currencyConverter = buildApplication();
+		System.out.println(currencyConverter.convert(amount, fromCode, toCode));
+	}
+
+	private static String readLine(String message) throws IOException {
+		System.out.print(message);
+		return new BufferedReader(new InputStreamReader(System.in)).readLine();
+	}
+
+	private static CurrencyConverter buildApplication() {
+		return new CurrencyConverter(new FileRateSupplier());
 	}
 }
